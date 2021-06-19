eb120e4b93144bfd3bee40cf7237c5bd9d0b55cd
JeffreySuarez <79020208+JeffreySuarez@users.noreply.github.com>
2021-06-19 09:48:36 -0500

    Subiendo carpetas

---
+++ src/components/CartWidget/cartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carshop from '../image/carshop.png'
+
+export const Card = () => {
+    return(
+        <div className='card'>
+            <div className='imagenLogo'>
+                <img  scr={Carshop} alt='LogoMafra' />
+            </div>
+        
+        </div>
+    )
+}
---
+++ src/components/ItemListContainer/itemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const Items = ({text}) => {
+
+    return(
+        <div>
+          <p>{text}</p>        
+        </div>
+    )
+}
